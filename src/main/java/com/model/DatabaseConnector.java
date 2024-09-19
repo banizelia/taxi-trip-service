70ab8cf50cc4f9985598c16b2ddd41e80e57add4
banizelia <vladslav468@gmail.com>
2024-09-19 20:07:39 +0500

    changed db password

--- src/main/java/com/model/DatabaseConnector.java
+++ src/main/java/com/model/DatabaseConnector.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DatabaseConnector {
     private static final String URL = "jdbc:postgresql://localhost:5432/project";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "100DjDf800";
+    private static final String PASSWORD = "12345678";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

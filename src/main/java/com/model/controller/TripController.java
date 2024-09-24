4503443d2db361954d3fab2ffd8e119bfdc168a0
banizelia <vladslav468@gmail.com>
2024-09-24 09:26:52 +0500

    latest non-working version

--- src/main/java/com/model/controller/TripController.java
+++ src/main/java/com/model/controller/TripController.java
@@ -2,6 +2,7 @@ package com.model.controller;
 
 import com.model.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.model.service.TripService;
 
@@ -24,43 +25,43 @@ public class TripController {
         return tripService.test();
     }
 
-//    @GetMapping("/filter")
-//    public Optional<List<Trip>> filterTrips(
-//            @RequestParam(value = "startDateTime", required = false) String startDateTime,
-//            @RequestParam(value = "endDateTime", required = false) String endDateTime,
-//            @RequestParam(value = "minWindSpeed", required = false) Double minWindSpeed,
-//            @RequestParam(value = "maxWindSpeed", required = false) Double maxWindSpeed,
-//            @RequestParam(value = "direction", required = false, defaultValue = "asc") String direction, /* asc or desc */
-//            @RequestParam(value = "sortBy", required = false, defaultValue = "id") String sortBy /* pickup_datetime or average_wind_speed */ ) {
-//
-//
-//        Optional<List<Trip>> result = tripService.filterTrips(startDateTime, endDateTime, minWindSpeed, maxWindSpeed , direction, sortBy);
-//
-//        return result;
-//    }
-//
-//    @PutMapping("/addToFavourite")
-//    public ResponseEntity<String> addToFavourite(@RequestParam(value = "id") Long id){
-////        try {
-//            tripService.addToFavourite(id);
-//            return ResponseEntity.ok("Success");
-////        } catch (Exception e) {
-////            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to add to favourites");
-////        }
-//    }
-//
-//    @DeleteMapping("/removeFromFavourite")
-//    public ResponseEntity<String> removeFromFavourite(@RequestParam(value = "id") Long id){
-////        try {
-//            tripService.removeFromFavourite(id);
-//            return ResponseEntity.ok("Success");
-////        } catch (Exception e) {
-////            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to remove from favourites");
-////        }
-//    }
-//
-//    @GetMapping("/getFavouriteList")
-//    public Optional<List<Trip>> getFavouriteList(){
-//        return tripService.getFavouriteList();
-//    }
+    @GetMapping("/filter")
+    public Optional<List<Trip>> filterTrips(
+            @RequestParam(value = "startDateTime", required = false) String startDateTime,
+            @RequestParam(value = "endDateTime", required = false) String endDateTime,
+            @RequestParam(value = "minWindSpeed", required = false) Double minWindSpeed,
+            @RequestParam(value = "maxWindSpeed", required = false) Double maxWindSpeed,
+            @RequestParam(value = "direction", required = false, defaultValue = "asc") String direction, /* asc or desc */
+            @RequestParam(value = "sortBy", required = false, defaultValue = "id") String sortBy /* pickup_datetime or average_wind_speed */ ) {
+
+
+        Optional<List<Trip>> result = tripService.filterTrips(startDateTime, endDateTime, minWindSpeed, maxWindSpeed , direction, sortBy);
+
+        return result;
+    }
+
+    @PutMapping("/addToFavourite")
+    public ResponseEntity<String> addToFavourite(@RequestParam(value = "id") Long id){
+//        try {
+            tripService.addToFavourite(id);
+            return ResponseEntity.ok("Success");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to add to favourites");
+//        }
+    }
+
+    @DeleteMapping("/removeFromFavourite")
+    public ResponseEntity<String> removeFromFavourite(@RequestParam(value = "id") Long id){
+//        try {
+            tripService.removeFromFavourite(id);
+            return ResponseEntity.ok("Success");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to remove from favourites");
+//        }
+    }
+
+    @GetMapping("/getFavouriteList")
+    public Optional<List<Trip>> getFavouriteList(){
+        return tripService.getFavouriteList();
+    }
 }
--- src/main/java/com/model/service/TripService.java
+++ src/main/java/com/model/service/TripService.java
@@ -14,7 +14,6 @@ public class TripService {
     @Autowired
     TripsRepository tripsRepository;
 
-
     public Optional<List<Trip>> test(){
 
 
@@ -23,13 +22,14 @@ public class TripService {
 
 
         for (Object[] objects : tripsRepository.test()) {
+
             Object[] tripParams =  Arrays.copyOfRange(objects, 0, 21);
-            Object[] weatherParams =  Arrays.copyOfRange(objects, 21, objects.length);
-
             Trip trip = new Trip(tripParams);
-            Weather weather = new Weather(weatherParams);
-            trip.setWeather(weather);
 
+            Object[] weatherParams =  Arrays.copyOfRange(objects, 21, objects.length);
+            Weather weather = new Weather(weatherParams);
+
+            trip.setWeather(weather);
             trips.add(trip);
         }
 
@@ -37,31 +37,51 @@ public class TripService {
     }
 
 
-//    public Optional<List<Trip>> filterTrips(String startDateTime, String endDateTime, Double minWindSpeed, Double maxWindSpeed, String direction, String sortBy) {
-//
-//
+    //     todo понять, почему соритровка выдает ошибку
 
-    // todo понять, почему соритровка выдает ошибку
+    // итоговый запрос выглядит вот так:
 
-//        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
-//
-//        List<Trip> trips = tripsRepository.filterTrips(startDateTime, endDateTime, minWindSpeed, maxWindSpeed /*, sort*/);
-//
-//        return Optional.of(trips);
-//    }
-//
-//
-//    public void addToFavourite(Long id) {
-//        tripsRepository.addToFavourite(id);
-//    }
-//
-//    public void removeFromFavourite(Long id) {
-//        tripsRepository.removeFromFavourite(id);
-//    }
-//
-//    public Optional<List<Trip>> getFavouriteList() {
-//        List<Trip> trips = tripsRepository.getFavouriteList();
-//
-//        return Optional.of(trips);
-//    }
+    // SELECT trips.*, weather_observations.* FROM weather_observations
+    // JOIN trips ON weather_observations.date = trips.pickup_datetime::DATE
+    // WHERE (? IS NULL OR pickup_datetime >= ?) AND
+    // (? IS NULL OR pickup_datetime <= ?) AND
+    // (? IS NULL OR weather_observations.average_wind_speed >= ?) AND
+    // (? IS NULL OR weather_observations.average_wind_speed <= ?)
+    // order by JOIN.average_wind_speed asc
+
+    // зачем добавляется "JOIN."
+    public Optional<List<Trip>> filterTrips(String startDateTime, String endDateTime, Double minWindSpeed, Double maxWindSpeed, String direction, String sortBy) {
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+
+        List<Trip> trips = new ArrayList<>();
+
+        for (Object[] objects : tripsRepository.filterTrips(startDateTime, endDateTime, minWindSpeed, maxWindSpeed, sort)) {
+
+            Object[] tripParams =  Arrays.copyOfRange(objects, 0, 21);
+            Trip trip = new Trip(tripParams);
+
+            Object[] weatherParams =  Arrays.copyOfRange(objects, 21, objects.length);
+            Weather weather = new Weather(weatherParams);
+
+            trip.setWeather(weather);
+            trips.add(trip);
+        }
+
+        return Optional.of(trips);
+    }
+
+
+    public void addToFavourite(Long id) {
+        tripsRepository.addToFavourite(id);
+    }
+
+    public void removeFromFavourite(Long id) {
+        tripsRepository.removeFromFavourite(id);
+    }
+
+    public Optional<List<Trip>> getFavouriteList() {
+        List<Trip> trips = tripsRepository.getFavouriteList();
+
+        return Optional.of(trips);
+    }
 }

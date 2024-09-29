5a275b85348789ac4e23344ce5445edd97191de1
banizelia <vladslav468@gmail.com>
2024-09-30 00:08:41 +0500

    better error handling and removed unnecessary  Optional<>

--- src/main/java/com/web/service/FavoriteTripService.java
+++ src/main/java/com/web/service/FavoriteTripService.java
@@ -1,13 +1,17 @@
 package com.web.service;
 
-import com.web.model.*;
+import com.web.model.FavoriteTrip;
+import com.web.model.Trip;
 import com.web.repository.FavoriteTripRepository;
+import jakarta.persistence.OptimisticLockException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FavoriteTripService {
@@ -21,34 +25,35 @@ public class FavoriteTripService {
                 return ResponseEntity.badRequest().body("Trip already in the table");
             }
 
-            Long maxPosition = favoriteTripRepository.findMaxPosition().orElse(0L);
+            Long maxPosition = favoriteTripRepository.findMaxPosition();
 
             FavoriteTrip favoriteTrip = new FavoriteTrip();
             favoriteTrip.setTripId(id);
-            favoriteTrip.setPosition(maxPosition+1);
+            favoriteTrip.setPosition(maxPosition + 1);
+
             favoriteTripRepository.save(favoriteTrip);
 
+        } catch (OptimisticLockException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict occurred while saving the trip");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error occurred while saving to favorite trips");
         }
-
-        return ResponseEntity.ok("success");
+        return ResponseEntity.status(HttpStatus.CREATED).body("Trip added to favorites");
     }
 
     public ResponseEntity<String> deleteFromFavourite(Long id) {
-        try {
-            FavoriteTrip favoriteTrip = favoriteTripRepository.getReferenceById(id);
-            favoriteTripRepository.delete(favoriteTrip);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("not in the table");
+        Optional<FavoriteTrip> favoriteTripOptional = favoriteTripRepository.findById(id);
+
+        if (favoriteTripOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Trip not found");
         }
 
-        return ResponseEntity.ok("success");
+        favoriteTripRepository.delete(favoriteTripOptional.get());
+        return ResponseEntity.ok("Trip deleted successfully");
     }
 
-    public Optional<List<Trip>> getFavouriteTrips() {
-        List<Trip> trips = favoriteTripRepository.getFavouriteTrips();
-        return Optional.of(trips);
+    public List<Trip> getFavouriteTrips() {
+        return favoriteTripRepository.getFavouriteTrips();
     }
 
 
@@ -60,6 +65,12 @@ public class FavoriteTripService {
 
             if (newPosition.equals(oldPosition)) {
                 return ResponseEntity.badRequest().body("Position is the same");
+            }
+
+            Long maxPosition = favoriteTripRepository.findMaxPosition();
+
+            if (newPosition < 0 || newPosition >= maxPosition) {
+                return ResponseEntity.badRequest().body("New position is out of bounds.");
             }
 
             if (newPosition < oldPosition) {

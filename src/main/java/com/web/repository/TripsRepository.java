1f3ba544ecd4efceff053ce3161ab315de8eaad7
banizelia <vladslav468@gmail.com>
2024-09-30 00:14:13 +0500

    added pagination and changed Timestamp to LocalDateTime

--- src/main/java/com/web/repository/TripsRepository.java
+++ src/main/java/com/web/repository/TripsRepository.java
@@ -2,12 +2,14 @@ package com.web.repository;
 
 import com.web.model.Trip;
 import org.springframework.data.domain.Limit;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Sort;
-import java.sql.Timestamp;
-import java.util.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TripsRepository extends JpaRepository<Trip, Long> {
@@ -15,11 +17,11 @@ public interface TripsRepository extends JpaRepository<Trip, Long> {
     @Query("SELECT t FROM Trip t WHERE " +
             " (pickupDatetime BETWEEN :startDateTime AND :endDateTime) AND " +
             " (weather.averageWindSpeed BETWEEN :minWindSpeed AND :maxWindSpeed)")
-    List<Trip> filter(@Param("startDateTime") Timestamp startDateTime,
-                      @Param("endDateTime") Timestamp endDateTime,
+    List<Trip> filter(@Param("startDateTime") LocalDateTime startDateTime,
+                      @Param("endDateTime") LocalDateTime endDateTime,
                       @Param("minWindSpeed") Double minWindSpeed,
                       @Param("maxWindSpeed") Double maxWindSpeed,
-                      Sort sort, Limit limit);
+                      Pageable pageable);
 
 
     @Query("select t from Trip t")

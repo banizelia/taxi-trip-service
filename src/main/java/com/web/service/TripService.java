39d80166a37288e5731b6bf3b4c8b83095afc382
banizelia <vladslav468@gmail.com>
2024-09-30 00:13:34 +0500

    added pagination and better error handling

--- src/main/java/com/web/service/TripService.java
+++ src/main/java/com/web/service/TripService.java
@@ -1,24 +1,47 @@
 package com.web.service;
 
-import com.web.util.ExcelHelper;
-import com.web.repository.TripsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
-import org.springframework.stereotype.Service;
 import com.web.model.Trip;
+import com.web.repository.TripsRepository;
+import com.web.util.ExcelHelper;
+import com.web.util.FieldUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
 import java.io.ByteArrayInputStream;
-import java.sql.Timestamp;
-import java.util.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
 public class TripService {
     @Autowired
     TripsRepository tripsRepository;
 
-    public Optional<List<Trip>> filter(Timestamp startDateTime, Timestamp endDateTime, Double minWindSpeed, Double maxWindSpeed, Sort sort) {
-        List<Trip> trips = tripsRepository.filter(startDateTime, endDateTime, minWindSpeed, maxWindSpeed, sort, Limit.of(500));
+    public List<Trip> filter(LocalDateTime startDateTime, LocalDateTime endDateTime, Double minWindSpeed, Double maxWindSpeed, String direction, String sortBy, Integer page) {
 
-        return Optional.of(trips);
+        if (endDateTime.isBefore(startDateTime)){
+            throw new IllegalArgumentException("endDateTime is before startDateTime: startDateTime = " + startDateTime + " endDateTime = " + endDateTime);
+        }
+
+        if (!direction.equalsIgnoreCase("asc") && !direction.equalsIgnoreCase("desc")){
+            throw new IllegalArgumentException("Invalid sort direction: " + direction);
+        }
+
+        if (!FieldUtil.getAllAllowedFields().contains(sortBy)) {
+            throw new IllegalArgumentException("Invalid sort field: " + sortBy);
+        }
+
+        if (page < 0){
+            throw new IllegalArgumentException("Invalid page field, smaller than zero: " + page);
+        }
+
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(page, 500, sort);
+
+        return tripsRepository.filter(startDateTime, endDateTime, minWindSpeed, maxWindSpeed, pageable);
     }
 
     public ByteArrayInputStream download() {

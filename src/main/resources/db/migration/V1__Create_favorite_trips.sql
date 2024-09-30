879ee400fd4f34513855789b77769ad97f625f73
banizelia <vladslav468@gmail.com>
2024-09-30 09:38:54 +0500

    flyway scripts

---
+++ src/main/resources/db/migration/V1__Create_favorite_trips.sql
@@ -0,0 +1,22 @@
+-- Version: V1__Create_favorite_trips.sql
+
+CREATE TABLE IF NOT EXISTS public.favorite_trips
+(
+    trip_id bigint NOT NULL,
+    position bigint,
+    version bigint,
+    CONSTRAINT favorite_trips_pkey PRIMARY KEY (trip_id)
+);
+
+ALTER TABLE IF EXISTS public.favorite_trips
+    OWNER TO postgres;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_favorite_trips_trip_id
+    ON public.favorite_trips USING btree
+    (trip_id ASC NULLS LAST);
+
+
+INSERT INTO public.favorite_trips (trip_id, position, version) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0);
